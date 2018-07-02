63b9e310fe8b15bafd45aa68a8343ba7a35a39f2
Lauren Perchitti <laurenperchitti@gmail.com>
2018-07-02 20:49:37 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,5 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-let index = 0
 
 function init(e) {
   // Write your JavaScript code inside the init() function
@@ -8,7 +7,7 @@ const key = parseInt(e.detail || e.which);
 
 if (key === code[index]){
   index++;
-  
+
 
   if (index === code.length){
     alert("yay!");
